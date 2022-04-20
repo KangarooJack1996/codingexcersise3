af84ddfdb38ab2c069520bdd5e600ddbb8006592
KangarooJack1996 <101534787+KangarooJack1996@users.noreply.github.com>
2022-04-19 21:28:25 -0400

    comiit

--- scripts.js
+++ scripts.js
@@ -13,3 +13,32 @@ function nav(e){
     $("ul.nav a").removeClass("active");
     $(this).addClass("active");
 }
+
+function search(e) {
+    if (e.key == "Enter") {
+      var search = $("#search").val();
+      console.log(search);
+      if (search.length < 3) {
+        alert("Keyword too short!")
+      } else {
+        var url = "https://api.openverse.engineering/v1/images/?q=" + search;
+        console.log(url);
+        $.ajax({
+          url: url,
+          success: populate
+        })
+      }
+    }
+  }
+  function populate(data) {
+      $("#result").empty();
+    $.each(data.results, place);
+  }
+  
+  function place(index, obj) {
+    
+    var html = "<img src='"+obj.thumbnail+"' class='mb-3 w-100'>";
+    $("#result").append(html);
+  }
+  $(document).on("keyup", "#search", search);
+  
